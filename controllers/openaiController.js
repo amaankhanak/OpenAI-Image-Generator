3d017300ba01c846d720b4321ee5e77e972bd9a5
amaankhanakz <sameeramaan2014@gmail.com>
2023-04-11 22:11:02 +0530

    project-start

---
+++ controllers/openaiController.js
@@ -0,0 +1,15 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+  try {
+    // 11:32
+  } catch (error) {}
+};
+
+module.exports = { generateImage };
