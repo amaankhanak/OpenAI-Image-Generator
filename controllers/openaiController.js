2e3c68d183aa019ca601c194828bd2208bb3a1a2
amaankhanakz <sameeramaan2014@gmail.com>
2023-04-14 10:43:37 +0530

    request data

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -8,8 +8,31 @@ const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
   try {
-    // trycatch
-  } catch (error) {}
+    const response = await openai.createImage({
+      prompt: "Polar bear on ice",
+      n: 1,
+      size: "512x512",
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: "The image could not be generated",
+    });
+  }
 };
 
 module.exports = { generateImage };
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// 15
+
 app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
